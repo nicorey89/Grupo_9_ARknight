2a4ae54094aaf0e8c6371be4ff6eadba067f2594
nicorey89 <nicolasthian8907@gmail.com>
2023-01-01 23:35:46 -0300

    creo estructura express

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const app = express ();
+const path = require ("path");
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+});
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+app.get("/productCard", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productCard.html"))
+});
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"))
+});
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+
+app.listen(PORT, () => 
+console.log("Servidor levantado en el puerto " + PORT));
