08a420b0491018f9a51c1dcacb5bd87a68025b24
nicorey89 <nicolasthian8907@gmail.com>
2023-02-03 17:39:23 -0300

    agrego readfilesync para ejecutar el json desde la vistay modifico la vista index para hacerla dinamica

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -1,6 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productos.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const writeJson = (products) => {
+	fs.writeFileSync(productsFilePath, JSON.stringify(products), 'utf-8')
+};
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+
 const controller ={
     index: (req, res)=>{
-       res.render('index');
+       res.render('index', {
+        products,
+        toThousand
+       });
     },
     login:(req, res)=>{
         res.render('users/login')
