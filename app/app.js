983a343c5ab0b21561778520e04c494ba74aa593
nicorey89 <nicolasthian8907@gmail.com>
2023-02-20 12:26:29 -0300

    descomentando codigo

--- app/app.js
+++ app/app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 3000;
-/* const methodOverride = require('method-override'); */
+const methodOverride = require('method-override');
 
 app.use(express.static("public"));
-/* app.use(methodOverride('_method')); */
+app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
