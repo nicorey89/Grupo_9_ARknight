961ce1d92f3656fbb2cf57869264f4cacb14766a
nicorey89 <nicolasthian8907@gmail.com>
2023-02-25 10:41:46 -0300

    agrego vista admincon su controlador y ruta

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/usersController');
 
-
+router.get('/admin', controller.admin)
 
 router.get('/login', controller.login);
 
