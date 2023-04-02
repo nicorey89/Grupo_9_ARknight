6b2fb71625f272b95db0606e1d3b86ac3207aac8
nicorey89 <nicolasthian8907@gmail.com>
2023-04-01 22:51:24 -0300

    realizo vista de listar usuarios y listar productos de admin

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const sessionAdminCheck = require("../middleware/sessionAdminCheck");   
 
-const {index, pAdmit, create,edit , destroy , store , update , listar} = require('../controllers/adminController')
+const {index, destroyUser, listarUsers, userUpdate, create,edit , destroy , store , update , listar} = require('../controllers/adminController')
 const {uploadFile} = require('../middleware/upload')
 
 
@@ -12,7 +12,8 @@ router.get("/", sessionAdminCheck ,index);
 /* GET - EDIT - ADMIN PRODUCT */
 
 router.get('/products',sessionAdminCheck , listar);
-router.get('/adminProducts/:id',sessionAdminCheck ,pAdmit);
+router.get('/users',sessionAdminCheck , listarUsers);
+router.put('/users/:id', userUpdate);
 
 /*** CREATE ONE PRODUCT ***/
 router.get('/create/', sessionAdminCheck ,create);
@@ -22,5 +23,6 @@ router.get('/edit/:id', sessionAdminCheck ,edit);
 router.put('/edit/:id', uploadFile.single('imagen') , update);
 /*** DELETE ONE PRODUCT ***/
 router.delete('/:id', destroy)
+router.delete('/users/:id', destroyUser)
 
 module.exports = router;
