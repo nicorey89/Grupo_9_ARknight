09d5265430dc4eb69fdf2083680acf64be94ee0c
Tomas Gongora <tomasgongora@hotmail.com>
2023-04-13 06:04:26 -0300

    agregando sequelize en productsControlles.js

--- app/controllers/productsController.js
+++ app/controllers/productsController.js
@@ -1,8 +1,10 @@
-const { readJSON, writeJSON } = require('../data')
+/* const { readJSON, writeJSON } = require('../data')
 
 
-const products = readJSON('productos.json');
+const products = readJSON('productos.json'); */
 
+ const {products,Sequelize} = require ('../database/modeLs')//, category}
+ const {Op} = Sequelize;
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -14,21 +16,36 @@ const controller = {
       session:req.session
     })
   },
+ 
   pDetail:(req, res)=>{
 
     let productId = req.params.id
     let product = products.find(product => product.id == productId)
     
-
-    return res.render("products/productDetail", {
+    const PRODUCT_PROMISE = Product.findByPk(product.id, {
+      include: [{association: 'images'}]
+    })
+    const ALL_PRODUCTS_PROMISE = Product.findALL({
+      where: {
+        discount: {
+          [Op.gte]: 10,
+        },
+      },
+      include: [{association: 'images'}]
+    });
+    Promise.all([PRODUCT_PROMISE, ALL_PRODUCTS_PROMISE])
+    .then(([product, sliderProducts]) => {
+      return res.render("products/productDetail", {
         ...product,
         toThousand,
         tittle : "Product Detail",
         sliderTitle: "PRODUCTOS EN OFERTAS",
-        sliderProducts: products,
-        session:req.session
+        sliderProducts,
+        session: req.session
         
     })
+    })
+    
     },
     pCard:(req, res)=>{
       let products = readJSON('productos.json')
