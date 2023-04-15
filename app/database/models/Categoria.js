22ee1fe3fbb1670ae78bec0aed3d5df80fd53616
nicorey89 <nicolasthian8907@gmail.com>
2023-04-15 15:21:49 -0300

    agregando modelos

---
+++ app/database/models/Categoria.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = "Categoria";
+
+    const cols = {
+        id: {
+            type: dataTypes.INTEGER(11),
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        nombre: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+        banner: {
+            type: dataTypes.STRING(100)
+        },
+    }
+
+    const config = {
+        tableName: "categorias",
+        timestamps: false,
+    }
+
+    const CATEGORIA = sequelize.define(alias, cols, config);
+
+    CATEGORIA.associate = (models) => {
+        PRODUCT_IMAGE.belongsTo(models.Subcategoria, {
+            as: "subcategorias",
+            foreignKey: "categoria_id"
+        });
+    }
+
+    return CATEGORIA;
+}
