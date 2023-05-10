e2187d276a2fe05ebc7ca72bd4d0a75a7826ef77
nicorey89 <nicolasthian8907@gmail.com>
2023-05-09 21:23:17 -0300

    solucionando problemas de apis y controladores

--- app/public/js/categorias.js
+++ app/public/js/categorias.js
@@ -6,12 +6,12 @@ window.addEventListener("load", ()=>{
         let categoriaId = event.target.value;
         console.log(categoriaId)
         fetch(`http://localhost:3000/api/v1/categoria/${categoriaId}`)
-        .then((res) => res.json().send())
-         .then((data) => {
+        .then((res) => res.json())
+        .then((data) => {
             selectSubcategorias.innerHTML = ""
-            console.log(data)
-            selectSubcategorias.innerHTML += `<option value='${data.Subcategorias.nombre}'>${data.Subcategorias.nombre}</option>`
-            
+            data.Subcategorias.forEach(subcategoria => {
+                selectSubcategorias.innerHTML += `<option value='${subcategoria.nombre}'>${subcategoria.nombre}</option>`
+            });
         }) 
         .catch((error) => console.log(error))
     })
