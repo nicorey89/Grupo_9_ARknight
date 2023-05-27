f2c527acf38424a77ed5f432756f600330966f42
Maxgg21 <lionel.gegena@gmail.com>
2023-05-27 12:57:53 -0300

    Amplementando Menu Desplegable con js

--- app/public/js/header.js
+++ app/public/js/header.js
@@ -1,3 +1,18 @@
+const boton = document.querySelector("#dropdown");
+const ventana = document.querySelector("#dropdown-content");
+
+function pullDown(){
+    ventana.classList.toggle("dropdown-block");
+}
+
+function pullDownCategories() {
+
+}
+
+
+
+
+
 window.addEventListener("scroll", () => {
     if(window.scrollY >= 100){
        document.querySelector(".main-header").classList.add("header-opacity")
@@ -5,3 +20,5 @@ window.addEventListener("scroll", () => {
        document.querySelector(".main-header").classList.remove("header-opacity")
    }
 })
+
+
