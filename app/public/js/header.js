9fc20b18d2991b770f441715ed56db78f5dbb407
Maxgg21 <lionel.gegena@gmail.com>
2023-05-27 13:18:55 -0300

    Agregando el subDesplegable con las categotias y funciones

--- app/public/js/header.js
+++ app/public/js/header.js
@@ -1,14 +1,21 @@
 const boton = document.querySelector("#dropdown");
 const ventana = document.querySelector("#dropdown-content");
 
+const itemsCategorias = document.querySelector("#itemsMenu");
+const categoryMenu = document.querySelector("#categoryMenu");
+
 function pullDown(){
     ventana.classList.toggle("dropdown-block");
-}
+};
 
 function pullDownCategories() {
+    categoryMenu.classList.toggle("pullDownCartegories")
+};
 
-}
+// itemsCategorias.addEventListener("click", () => {
+//     console.log(itemsCategorias);
 
+// })
 
 
 
